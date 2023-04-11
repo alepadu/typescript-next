f87e993b322e926420a82c855934679a5e919926
alepadu <76449254+alepadu@users.noreply.github.com>
2023-04-12 01:00:21 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -3,4 +3,6 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
+const appDiv2: HTMLElement = document.getElementById('app2');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv2.outerHTML = `<h2>Ciao!</h2>`;
